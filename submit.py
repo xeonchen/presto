eb33227263250ffec70887db35d2e5656cd623f8
Liang-Heng Chen <xeonchen@gmail.com>
2017-03-13 11:30:56 +0800

    refinement

--- submit.py
+++ submit.py
@@ -19,95 +19,85 @@ import urllib
 import json
 import os.path
 import sys
-from datetime import datetime, date, time
+from datetime import datetime
 
 def parse_config():
+    global config
+
     with open('config.toml', 'rb') as fin:
         obj = pytoml.load(fin)
-    if not obj['key']:
-        raise Exception('Key must be defined in config file')
-    if not 'start' in obj:
-        obj['start'] = 0
-    if not 'end' in obj:
-        obj['end'] = 10000
-    return obj
+
+    config = { 'start': 0, 'end': 10000 }
+    config.update(obj)
 
 def parse_domains():
-    tests = []
     with open('top100.txt', 'r') as fin:
-        tests = [x.strip('\n') for x in fin.readlines()]
-    return tests
+        return [x.strip('\n') for x in fin.readlines()]
 
-def log_response_id(files, location, index, domain, test_id, owner_key):
-    f = None
-    if not location in files:
-        filename = location+"_"+datetime.now().strftime("%m-%d-%H%M")+".csv"
-        if not os.path.exists(filename):
-            f = open(filename, 'w')
-        else:
-            f = open(filename, 'a+')
-            f.seek(0, 2) # go to end
+def log_response_id(location, index, domain, test_id, owner_key):
+    f = logs[location]
+    line = '%s, %s, %s, %s\n' % (index, domain, test_id, owner_key)
+    f.write(line)
 
-        files[location] = f
-    else:
-        f = files[location]
-    f.write(str(index) + ", "+ domain + ", " + test_id + ", " + owner_key + "\n")
+def get_param(location, url):
+    params = {
+        'f': 'json',
+        'location': location,
+        'pingback': config['pingback'],
+        'priority' : config.get('priority', '5'),
+        'runs': config['runs'],
+        'url': url
+    }
+    if config.get('key'):
+        params['k'] = config['key']
 
+    if config.get('label'):
+        params['label'] = config['label']
 
-def main():
-    config = parse_config()
-    print config
+    return urllib.urlencode(params)
 
+def submit_all():
     tests = parse_domains()
-    results = {}
-
-    files = {}
 
     for i in range(config['start'], config['end']):
         for location in config['locations']:
-            if len(config['label'])>0:
-                params = urllib.urlencode({'k': config['key'], 'location': location, 'url': tests[i], 'f': 'json', 'runs': 10, 'pingback': config['pingback'], 'label': config['label']})
-            else:
-                params = urllib.urlencode({'k': config['key'], 'location': location, 'url': tests[i], 'f': 'json', 'runs': 10, 'pingback': config['pingback']})
-            f = urllib.urlopen(config['endpoint'], params)
-            response = json.loads(f.read())
-            print response
-            log_response_id(files, location, i, tests[i], response['data']['testId'], response['data']['ownerKey'])
+            submit_one(tests[i], location, i)
 
-    for key in files:
-        files[key].close()
+def submit_one(domain, location, index=0, output=None):
+    if location not in logs:
+        if output is None:
+            output = '%s_%s.csv' % (location, datetime.now().strftime("%m-%d-%H%M"))
 
-def submit_one():
-    config = parse_config()
-    # domain, location, output
-    domain = sys.argv[1]
-    location = sys.argv[2]
-    output = sys.argv[3]
-    files = {}
+        if not os.path.exists(output):
+            f = open(output, 'w')
+        else:
+            f = open(output, 'a+')
+            f.seek(0, 2) # go to end
 
-    if not os.path.exists(output):
-        f = open(output, 'w')
-    else:
-        f = open(output, 'a+')
-        f.seek(0, 2) # go to end
+        logs[location] = f
 
-    files[location] = f
-
-    if len(config['label'])>0:
-        params = urllib.urlencode({'k': config['key'], 'location': location, 'url': domain, 'f': 'json', 'runs': 10, 'pingback': config['pingback'], 'label': config['label']})
-    else:
-        params = urllib.urlencode({'k': config['key'], 'location': location, 'url': domain, 'f': 'json', 'runs': 10, 'pingback': config['pingback']})
-    f = urllib.urlopen(config['endpoint'], params)
+    param = get_param(location, domain)
+    print param
+    f = urllib.urlopen(config['endpoint'], param)
     response = json.loads(f.read())
-    log_response_id(files, location, 0, domain, response['data']['testId'], response['data']['ownerKey'])
     print response
+    if response['statusCode'] == 200:
+        log_response_id(location, 0, domain, response['data']['testId'], response['data']['ownerKey'])
 
-    for key in files:
-        files[key].close()
+def main(args):
+    global logs
+    logs = {}
+
+    parse_config()
+
+    if len(args) == 1:
+        submit_all()
+    elif len(args) == 4:
+        submit_one(args[1], args[2], args[3])
+
+    for f in logs.itervalues():
+        f.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        main()
-    else:
-        submit_one()
+    main(sys.argv)
 
